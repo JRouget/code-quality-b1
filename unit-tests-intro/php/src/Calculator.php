37fbc935638210a75620a9b873d6fd29acb69347
Yoan Thirion <yoan.thirion@gmail.com>
2025-12-10 18:08:39 +0100

    feat: add unit test for intro

--- unit-tests-intro/php/src/Calculator.php
+++ unit-tests-intro/php/src/Calculator.php
@@ -1,27 +1,41 @@
 <?php
 
-abstract class Operators {
-    const Add = "add";
-    const Multiply = "multiply";
-    const Divide = "divide";
-    const Substract = "substract";
+abstract class Operators
+{
+    const ADD = "add";
+    const MULTIPLY = "multiply";
+    const DIVIDE = "divide";
+    const SUBSTRACT = "substract";
 }
 
-class Calculator {
+class Calculator
+{
+    private array $supportedOperators;
 
-    private $supportedOperators;
-
-    public function __construct() {
+    public function __construct()
+    {
         $this->supportedOperators = [
-            Operators::Add       => function ($a, $b) { return $a + $b; },
-            Operators::Multiply  => function ($a, $b) { return $a * $b; },
-            Operators::Divide    => function ($a, $b) { return $a / $b; },
-            Operators::Substract => function ($a, $b) { return $a - $b; }
+            Operators::ADD => function ($a, $b) {
+                return $a + $b;
+            },
+            Operators::MULTIPLY => function ($a, $b) {
+                return $a * $b;
+            },
+            Operators::DIVIDE => function ($a, $b) {
+                return $a / $b;
+            },
+            Operators::SUBSTRACT => function ($a, $b) {
+                return $a - $b;
+            }
         ];
     }
 
-    public function calculate($a, $b, $operator) {
-        if(!array_key_exists($operator, $this->supportedOperators)) {
+    /**
+     * @throws Exception
+     */
+    public function calculate($a, $b, $operator)
+    {
+        if (!array_key_exists($operator, $this->supportedOperators)) {
             throw new Exception("Unsupported operator {$operator}");
         }
         return $this->supportedOperators[$operator]($a, $b);
